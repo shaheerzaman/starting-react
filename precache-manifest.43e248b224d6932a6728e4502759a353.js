d1d6c5cb6966af5e53f05a82f37b8d286fb9a6da
shaheerzaman <shaheer_zaman84@yahoo.com>
2020-10-19 19:54:33 +0530

    Updates

---
+++ precache-manifest.43e248b224d6932a6728e4502759a353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65397554de2d1e9be5b79dd82dde3093",
+    "url": "/starting-react/index.html"
+  },
+  {
+    "revision": "cc405bf4393c26efa28f",
+    "url": "/starting-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30c4affd6e85b0055f5b",
+    "url": "/starting-react/static/js/2.3f4402a6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/starting-react/static/js/2.3f4402a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc405bf4393c26efa28f",
+    "url": "/starting-react/static/js/main.5eeac23d.chunk.js"
+  },
+  {
+    "revision": "cbe0223f76f3889817ae",
+    "url": "/starting-react/static/js/runtime-main.5bc147b9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/starting-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
